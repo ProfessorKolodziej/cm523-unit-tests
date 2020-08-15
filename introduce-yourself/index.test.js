706be620b9e16044aa16d96ba713c37746f04c68
Ashley Kolodziej <kolodz10@bu.edu>
2020-08-15 18:35:44 -0400

    Check for one and only one h1

--- introduce-yourself/index.test.js
+++ introduce-yourself/index.test.js
@@ -3,39 +3,20 @@ import '@testing-library/jest-dom/extend-expect'
 import { JSDOM } from 'jsdom'
 import fs from 'fs'
 import path from 'path'
+import '../extend-expect-cm523'
 
 const html = fs.readFileSync(path.resolve('src', './index.html'), 'utf8');
 
 let dom
 let container
 
-expect.extend({
-  toHaveOneH1( received ) {
-    const pass = received.querySelector('h1').length === '1';
-
-    if ( pass ) {
-      return {
-        message: () =>
-          `There is one and only one H1 element in the document.`,
-        pass: true,
-      };
-    } else {
-      return {
-        message: () =>
-          `You should only have one H1 on this page.`,
-        pass: false,
-      };
-    }
-  },
-});
-
 describe('index.html', () => {
-  beforeEach(() => {
-    dom = new JSDOM( html )
-    container = dom.window.document.body
-  })
+	beforeEach(() => {
+		dom = new JSDOM( html )
+		container = dom.window.document.body
+	})
 
-  test('renders a heading element', () => {
-    expect( container ).toHaveOneH1()
-  })
+	test('Has one h1 element', () => {
+		expect( container ).toHaveOneH1()
+	})
 })
