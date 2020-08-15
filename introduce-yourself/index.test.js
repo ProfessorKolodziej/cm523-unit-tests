3a02dbf704845a1c8dc14f4b4092f19a3d6bfd5d
Ashley Kolodziej <kolodz10@bu.edu>
2020-08-15 18:11:42 -0400

    Custom h1 test

--- introduce-yourself/index.test.js
+++ introduce-yourself/index.test.js
@@ -9,6 +9,26 @@ const html = fs.readFileSync(path.resolve('src', './index.html'), 'utf8');
 let dom
 let container
 
+expect.extend({
+  toHaveOneH1(received) {
+    const pass = received.querySelector('h1').length === '1';
+
+    if ( pass ) {
+      return {
+        message: () =>
+          `There is one and only one H1 element in the document.`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `You should only have one H1 on this page.`,
+        pass: false,
+      };
+    }
+  },
+});
+
 describe('index.html', () => {
   beforeEach(() => {
     dom = new JSDOM( html )
@@ -16,6 +36,6 @@ describe('index.html', () => {
   })
 
   test('renders a heading element', () => {
-    expect(container.querySelector('h1')).not.toBeNull()
+    expect(container.toHaveOneH1())
   })
 })
