e8039c5c761932310c0a405a922e3a4584c27012
ashleykolodziej <kolodz10@bu.edu>
2021-08-27 00:15:01 -0400

    Add html linter for final project

---
+++ final-project/index.test.js
@@ -0,0 +1,20 @@
+import { fireEvent, getByText } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+import "html-validate/jest"
+import '../extend-expect-cm523'
+
+const html = fs.readFileSync(path.resolve('', './src/index.html'), 'utf8'),
+		dom = new JSDOM( html ),
+		container = dom.window.document.body;
+
+describe('index.html', () => {
+	test('All HTML is valid', () => {
+		expect( html ).toHTMLValidate( {
+			extends: ["html-validate:standard"],
+			root: true
+		} );
+	} );
+})
